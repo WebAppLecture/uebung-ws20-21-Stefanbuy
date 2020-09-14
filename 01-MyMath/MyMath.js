a10ac96a2d46cbdf7c4fff9beb92211d61a2d31b
Stefanbuy <53040360+Stefanbuy@users.noreply.github.com>
2020-09-14 21:46:42 +0200

    Beutifyed version

--- 01-MyMath/MyMath.js
+++ 01-MyMath/MyMath.js
@@ -15,71 +15,71 @@ export class MyMath {
         // 'this' referenziert den Kontext in dem die aktuelle Funktion aufgerufen wird. 
         // Hier referenziert es die Instanz der Klasse MyMath die wir gerade erstellen.
         // mit 'value * 1' erzwingen wir, dass value als number gelesen wird.
-        if(value === undefined){
+        if (value === undefined) {
             this.value = 0;
-        }else{this.value = value * 1;}
+        } else {
+            this.value = value * 1;
+        }
         this.result;
     }
-    //return einbauen
+
     add(value2) {
-        this.result =this.value+value2;
-        this.value = this.result; 
-    return this;
+        this.result = this.value + value2;
+        this.value = this.result;
+        return this;
     }
 
     subtract(value2) {
-    this.result =this.value-value2; 
-    //console.log(this.result);
-    this.value = this.result; 
-    return this;
+        this.result = this.value - value2;
+        this.value = this.result;
+        return this;
     }
 
     multiply(value2) {
-        this.result =this.value*value2; 
-    //    console.log(this.result);
-    this.value = this.result; 
-    return this;
+        this.result = this.value * value2;
+        this.value = this.result;
+        return this;
     }
 
     divide(value2) {
-        if(value2 === 0){
-            this.result =this.value; 
-        }else{
-            this.result =this.value/value2; 
+        if (value2 === 0) {
+            this.result = this.value;
+        } else {
+            this.result = this.value / value2;
         }
-        
-        //console.log(this.result);
-        this.value = this.result; 
+
+        this.value = this.result;
         return this;
     }
 
     pow(value2) {
-        if(value2<0){console.log("no negatives");}else{
+        if (value2 < 0) {
+            console.log("no negatives");
+        } else {
             this.result = this.value;
-            for(let i = 1;i<value2;i++){
+            for (let i = 1; i < value2; i++) {
                 this.result = this.result * this.result;
             }
-//            console.log(this.result);
         }
-        this.value = this.result; 
+        this.value = this.result;
         return this;
     }
 
     faculty() {
-        //`n! = n ⋅ (n - 1) ⋅ (n - 2) ⋅ ... ⋅ 1`
         let num = this.value;
-        
+
         this.result = num;
-        while(num >1){
+        while (num > 1) {
             console.log("test");
-            if(num % 1 !== 0){break;}
+            if (num % 1 !== 0) {
+                break;
+            }
             console.log(num);
-            num =num-1;
+            num = num - 1;
             this.result = this.result * num;
         }
-        //console.log(this.result);
-        this.value = this.result; 
+        this.value = this.result;
         return this;
     }
-    
+
 }
